89d49ac400d1df8a5a3248cfda58812e8026f5e1
kalyaninagaraj <kalyanin@gmail.com>
2022-07-08 14:56:14 +0530

    Update genXML.py

--- CODE/genXML.py
+++ CODE/genXML.py
@@ -26,6 +26,8 @@ from bs4 import CData
 parser = argparse.ArgumentParser()
 parser.add_argument("action", help="specify data file for generating XML")
 parser.add_argument("target", help="specify target XML file name")
+parser.add_argument("--alg", help="specify algorithm for solving TSP", default="lk")
+
 args = parser.parse_args()
 
 try:
@@ -37,21 +39,23 @@ try:
         city_coord += buffer
     f.close()
 except OSError as e:
-    sys.stderr.write("I/O error(%d): %s: %s\n" % (e.errno, args.action, e.strerror))
+    sys.stderr.write("I/O error(%d): %s : %s\n" % (e.errno, args.action, e.strerror))
     sys.exit(1)
 
 myemail = input('Enter a valid email to submit the job on NEOS: ')
 
-bs_data  = BeautifulSoup("<document><category>co</category><solver>concorde</solver>\
+bs_data  = BeautifulSoup(
+"<document><category>co</category><solver>concorde</solver>\
 <inputMethod>TSP</inputMethod><email></email><priority></priority><dat2></dat2>\
-<dat1></dat1><tsp></tsp><ALGTYPE></ALGTYPE><RDTYPE></RDTYPE><PLTYPE></PLTYPE></document>", 'xml')
+<dat1></dat1><tsp></tsp><ALGTYPE></ALGTYPE><RDTYPE></RDTYPE><PLTYPE></PLTYPE>\
+</document>", 'xml')
 
-bs_data.dat2.insert(0, CData(city_coord))
-bs_data.priority.insert(0, CData('long'))
-bs_data.email.insert(0, CData(myemail))
-bs_data.ALGTYPE.insert(0, CData('lk'))
-bs_data.RDTYPE.insert(0, CData('variable'))
-bs_data.PLTYPE.insert(0, CData('no'))
+bs_data.dat2.insert(0, CData(city_coord))   # use dat2 if distances are measured in L2 norm, and dat1 if using L1 norm
+bs_data.priority.insert(0, CData('long'))   # long priority = longer runtime
+bs_data.email.insert(0, CData(myemail))     # valid email required by NEOS
+bs_data.ALGTYPE.insert(0, CData(args.alg))  # cqs=QSopt, con=CPLEX, lk=Lin-Kernighan (default)
+bs_data.RDTYPE.insert(0, CData('variable')) # random seed (fixed=99 [default], variable=random)
+bs_data.PLTYPE.insert(0, CData('no'))       # Plot optimal tour? (no [default], cp=yes (to PDF), pf=w/o cities (to PDF))
 
 try:
     with open(args.target, 'w') as f:
